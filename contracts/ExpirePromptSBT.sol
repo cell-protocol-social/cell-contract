e72fb5a761e30f7566fb544ead03c04fff76c7f8
brooks <brooks.cell@outlook.com>
2023-07-14 18:45:08 +0800

    feat: :sparkles: update ExpirePromptSBT with interface

--- contracts/ExpirePromptSBT.sol
+++ contracts/ExpirePromptSBT.sol
@@ -2,74 +2,30 @@
 pragma solidity 0.8.16;
 
 import "./base/BaseSBT.sol";
-
-interface IExpirePromptSBT {
-
-    /**
-     * @notice Mint a new token only by the owner or the trust singer
-     *         the token will not expire
-     * 
-     * @param to The address to mint to
-     * @param tokenId The token ID to mint
-     * @param _tokenURI The token URI of the token ID
-     */
-    function mint(address to, uint256 tokenId, string memory _tokenURI) external;
-
-    /**
-     * @notice Mint a expireTime token only by the owner or the trust singer
-     * 
-     * @param to The address to mint to
-     * @param tokenId The token ID to mint
-     * @param _tokenURI The token URI of the token ID
-     * @param expireTime The expire time of the token ID
-     */
-    function mint(address to, uint256 tokenId, string memory _tokenURI, uint256 expireTime) external;
-
-    /**
-     * @notice Renew a expireTime token only by the owner or the trust singer
-     * 
-     * @param tokenId The token ID to renew
-     * @param _tokenURI The token URI of the token ID
-     * @param expireTime The expire time of the token ID to renew
-     */
-    function renew(uint256 tokenId, string memory _tokenURI, uint256 expireTime) external;
-
-    /**
-     * @notice Get the expire time of the token ID
-     * 
-     * @param tokenId The token ID to check
-     * @return The expire time of the token ID
-     */
-    function expireOf(uint256 tokenId) external view returns (uint256);
-
-    /**
-     * @notice Check the token ID is expired
-     * 
-     * @param tokenId The token ID to check
-     * @return True if the token ID is expired
-     */
-    function isExpire(uint256 tokenId) external view returns (bool);
-}
+import "./interfaces/IExpirePromptSBT.sol";
 
 contract ExpirePromptSBT is IExpirePromptSBT, BaseSBT {
-    address public trustSinger;
+    address public trustFactory;
     mapping(uint256 => uint256) internal _expireOfTokenId;
+
+    error NotOwnerOf();
+    error NotOwnerOrFromTrust();
 
     function initialize(
         string memory name_,
         string memory symbol_,
         string memory baseTokenURI_,
-        address trustSinger_
+        address trustFactory_
     ) initializer external {
         __BaseSBT_init(name_, symbol_, baseTokenURI_);
-        trustSinger = trustSinger_;
+        trustFactory = trustFactory_;
     }
 
     /**
      * @inheritdoc IExpirePromptSBT
      */
     function mint(address to, uint256 tokenId, string memory _tokenURI) external override virtual {
-        _requireOwnerOrTrustSinger(); 
+        _requireOwnerOrFromTrust(); 
         _mintTrust(to, tokenId, _tokenURI);
     }
     
@@ -77,22 +33,43 @@ contract ExpirePromptSBT is IExpirePromptSBT, BaseSBT {
      * @inheritdoc IExpirePromptSBT
      */
     function mint(address to, uint256 tokenId, string memory _tokenURI, uint256 expireTime) external override virtual {
-        _requireOwnerOrTrustSinger(); 
+        _requireOwnerOrFromTrust(); 
         _expireOfTokenId[tokenId] = expireTime;
         _mintTrust(to, tokenId, _tokenURI);
     } 
+
+    function burn(uint256 tokenId) external override virtual {
+        if (!_isApprovedOrOwner(_msgSender(), tokenId)) revert NotApprovedOrOwnerOf();
+
+        _expireOfTokenId[tokenId] = 0;
+        _burn(tokenId);
+    }
+
+    /**
+     * @inheritdoc IExpirePromptSBT
+     */
+    function burnFrom(address to, uint256 tokenId) external override virtual {
+        if (to != ownerOf(tokenId)) revert NotOwnerOf();
+        if (
+            !_isApprovedOrOwner(_msgSender(), tokenId) && 
+            !(_msgSender() == trustFactory)
+        ) revert NotApprovedOrOwnerOf();
+
+        _expireOfTokenId[tokenId] = 0;
+        _burn(tokenId);
+    }
 
     /**
      * @inheritdoc IExpirePromptSBT
      */
     function renew(uint256 tokenId, string memory _tokenURI, uint256 expireTime) external override virtual {
-        _requireOwnerOrTrustSinger();
+        _requireOwnerOrFromTrust();
         _expireOfTokenId[tokenId] = expireTime;
         _setTokenURI(tokenId, _tokenURI);
     }
 
-    function setTrustSinger(address trustSinger_) external onlyOwner {
-        trustSinger = trustSinger_;
+    function setTrustFactory(address trustFactory_) external onlyOwner {
+        trustFactory = trustFactory_;
     }
 
     function expireOf(uint256 tokenId) external view returns (uint256) {
@@ -103,11 +80,11 @@ contract ExpirePromptSBT is IExpirePromptSBT, BaseSBT {
         return _expireOfTokenId[tokenId] != 0 && _expireOfTokenId[tokenId] <= block.timestamp;
     }
 
-    function _requireOwnerOrTrustSinger() internal view {
+    function _requireOwnerOrFromTrust() internal view {
         if (
             _msgSender() != owner() &&
-            _msgSender() != trustSinger
-        ) revert NotOwnerOrApproval();
+            _msgSender() != trustFactory
+        ) revert NotOwnerOrFromTrust();
     }
 
     function _mintTrust(address to, uint256 tokenId, string memory _tokenURI) internal virtual {
---
+++ contracts/interfaces/IExpirePromptSBT.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+interface IExpirePromptSBT {
+
+    /**
+     * @notice Mint a new token only by the owner or the trust singer
+     *         the token will not expire
+     * 
+     * @param to The address to mint to
+     * @param tokenId The token ID to mint
+     * @param _tokenURI The token URI of the token ID
+     */
+    function mint(address to, uint256 tokenId, string memory _tokenURI) external;
+
+    /**
+     * @notice Mint a expireTime token only by the owner or the trust singer
+     * 
+     * @param to The address to mint to
+     * @param tokenId The token ID to mint
+     * @param _tokenURI The token URI of the token ID
+     * @param expireTime The expire time of the token ID
+     */
+    function mint(address to, uint256 tokenId, string memory _tokenURI, uint256 expireTime) external;
+
+    /**
+     * @notice Renew a expireTime token only by the owner or the trust singer
+     * 
+     * @param tokenId The token ID to renew
+     * @param _tokenURI The token URI of the token ID
+     * @param expireTime The expire time of the token ID to renew
+     */
+    function renew(uint256 tokenId, string memory _tokenURI, uint256 expireTime) external;
+
+    /**
+     * @notice Burn a token from the trust factory contract or owner
+     *         require the to is the owner of the token ID
+     * 
+     * @param to The address to burn tokenId owner
+     * @param tokenId The token ID to burn
+     */
+    function burnFrom(address to, uint256 tokenId) external;
+
+    /**
+     * @notice Get the expire time of the token ID
+     * 
+     * @param tokenId The token ID to check
+     * @return The expire time of the token ID
+     */
+    function expireOf(uint256 tokenId) external view returns (uint256);
+
+    /**
+     * @notice Check the token ID is expired
+     * 
+     * @param tokenId The token ID to check
+     * @return True if the token ID is expired
+     */
+    function isExpire(uint256 tokenId) external view returns (bool);
+}
